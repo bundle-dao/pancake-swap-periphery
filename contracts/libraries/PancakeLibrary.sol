153df8e7b0a1f804b0278336a0df37ce8f72d3ae
Bundler <dev@bundledao.org>
2021-06-20 17:14:22 -0700

    Fix library

--- contracts/libraries/PancakeLibrary.sol
+++ contracts/libraries/PancakeLibrary.sol
@@ -16,13 +16,7 @@ library PancakeLibrary {
 
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66' // init code hash
-            ))));
+        pair = IPancakeFactory(factory).getPair(tokenA, tokenB);
     }
 
     // fetches and sorts the reserves for a pair
